21ff6ceb3cdf47b605b4704e5238295a2a2bf132
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-06 16:42:06 -0300

    Create atv9_2.py

---
+++ atv9_2.py
@@ -0,0 +1,5 @@
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+delta = B**2 - 4 * A * C
+print(f"O valor de Delta é {delta}.")
