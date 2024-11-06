7fb168759169253b90e25a0ad10a7e5fb3887e70
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-06 16:43:23 -0300

    Create atv9_4.py

---
+++ atv9_4.py
@@ -0,0 +1,4 @@
+salario = float(input("Digite o salário do funcionário: R$"))
+aumento = salario * 0.15
+novo_salario = salario + aumento
+print(f"O novo salário com 15% de aumento é R${novo_salario:.2f}.")
