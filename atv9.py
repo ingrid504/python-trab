5cbd03f84298c4b725dcd0befc860e5de474f23f
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-13 15:18:11 -0300

    Update atv9.py

--- atv9.py
+++ atv9.py
@@ -1,2 +1,8 @@
-]dinheiro = float(input("Quanto dinheiro você tem na carteira? R$"))
-print(f"Você tem R${dinheiro:.2f} na carteira.")
+#CONVERSAO DE REAL PARA DOLAR
+real = float(input("Informe o valor em R$: "))
+cotacao = 5.79
+dolar = real / cotacao
+print(f"O valor de R$ {real} é igual a US {dolar:.2f}")
+R:Informe o valor em R$: 
+3.50
+O valor de R$ 3.5 é igual a US 0.60
