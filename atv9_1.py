0169af1307fbde7745110fb2e0df246db70ab35d
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-13 15:22:45 -0300

    Update atv9_1.py

--- atv9_1.py
+++ atv9_1.py
@@ -1,6 +1,7 @@
 largura = float(input("Digite a largura da parede em metros: "))
 altura = float(input("Digite a altura da parede em metros: "))
 area = largura * altura
-tinta_necessaria = area / 2
-print(f"A área da parede é {area} metros quadrados.")
-print(f"Você precisará de {tinta_necessaria} litros de tinta para pintá-la.")
+quant_litros area / 2
+print("Será necessário", quant_litros, "litros.")
+
+
