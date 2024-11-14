8ba717ec007754eeab15c4710ca921737ebf85ab
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-14 15:01:57 -0300

    Update atv1_7.py

--- atv1_7.py
+++ atv1_7.py
@@ -1,7 +1,4 @@
-cigarros_por_dia = int(input("Digite a quantidade de cigarros fumados por dia: "))
-anos_fumando = int(input("Digite a quantidade de anos que você já fuma: "))
-minutos_perdidos_por_cigarro = 10
-total_cigarros = cigarros_por_dia * anos_fumando * 365
-total_minutos_perdidos = total_cigarros * minutos_perdidos_por_cigarro
-dias_perdidos = total_minutos_perdidos / (24 * 60)
-print(f"Você perdeu aproximadamente {dias_perdidos:.2f} dias de vida devido ao fumo.")
+velocidade= float(input("informe a velocidade "))
+if velocidade > 80 :
+    multa = (velocidade - 80) * 5
+    print(f"Velocidade acima de 80 km - multa R$ {multa:.2f} ")
