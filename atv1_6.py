5468911106085216ab80cfa55e3240387f2a4f7c
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-14 15:01:09 -0300

    Update atv1_6.py

--- atv1_6.py
+++ atv1_6.py
@@ -1,5 +1,7 @@
-dias_trabalhados = int(input("Digite o número de dias trabalhados no mês: "))
-horas_por_dia = 8
-salario_por_hora = 25
-salario_total = dias_trabalhados * horas_por_dia * salario_por_hora
-print(f"O salário total do mês é R${salario_total:.2f}.")
+cigarros_por_dia = int(input("Digite a quantidade de cigarros fumados por dia: "))
+anos_fumando = int(input("Digite a quantidade de anos que você já fuma: "))
+minutos_perdidos_por_cigarro = 10
+total_cigarros = cigarros_por_dia * anos_fumando * 365
+total_minutos_perdidos = total_cigarros * minutos_perdidos_por_cigarro
+dias_perdidos = total_minutos_perdidos / (24 * 60)
+print(f"Você perdeu aproximadamente {dias_perdidos:.2f} dias de vida devido ao fumo.")
