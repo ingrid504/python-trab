207ca81d6e282135ed7e9dda264f0301f42c75be
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-14 14:58:31 -0300

    Update atv1_4.py

--- atv1_4.py
+++ atv1_4.py
@@ -3,6 +3,6 @@ dias_alugados = int(input("Digite a quantidade de dias pelos quais o carro foi a
 preco_km = km_percorridos * 0.20
 preco_dias = dias_alugados * 90
 preco_total = preco_km + preco_dias
-print(f"O preço total a pagar é R${preco_total:.2f}.").
+print(f"O preço total a pagar é R${preco_total:.2f}.")
 
 
