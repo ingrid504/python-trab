3b3105f673a1bd5d2847f6abf79238488ce59e41
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-18 21:09:39 -0300

    Update atv2_88.py

--- atv2_88.py
+++ atv2_88.py
@@ -1,15 +1,10 @@
-nome = input("Digite o nome: ")
-salario = float(input("Digite o salario: "))  # Converte o salário para float
-anos = int(input("Digite quantos anos trabalha na empresa: "))  # Converte o número de anos para int
+largura = float(input("Informe a largura do terreno: "))
+comprimento = float(input("Informe o comprimento do terreno: "))
+area = largura * comprimento
 
-if anos <= 3:
-    aumento = salario * 0.03
-    faixa_aumento = "3%"
-elif 3 < anos < 10:
-    aumento = salario * 0.12
-    faixa_aumento = "12%"
+if area > 100:
+    print("Área do terreno é", area, "m. Terreno popular.")
+elif area >= 100 and comprimento < 500:
+    print("Área do terreno é", area, "m. Terreno master.")
 else:
-    aumento = salario * 0.20
-    faixa_aumento = "20%"
-
-novo_salario = salario + aumento   
+    print("Área do terreno é", area, "m. Terreno VIP.")
