723b78ad71ff925f8a63f321cfb9759d02a2d04b
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-18 21:09:13 -0300

    Update atv2_7.py

--- atv2_77.py
+++ atv2_77.py
@@ -1,10 +1,16 @@
-largura = float(input("Informe a largura do terreno: "))
-comprimento = float(input("Informe o comprimento do terreno: "))
-area = largura * comprimento
+# Função para calcular a média e mostrar a situação do aluno
+def calcular_media():
+    # Leitura das duas notas
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    media = (nota1 + nota2) / 2
 
-if area > 100:
-    print("Área do terreno é", area, "m. Terreno popular.")
-elif area >= 100 and comprimento < 500:
-    print("Área do terreno é", area, "m. Terreno master.")
-else:
-    print("Área do terreno é", area, "m. Terreno VIP.")
+    print(f"\nA média do aluno é: {media:.1f}")
+    if media <= 4.9:
+        print("Situação: REPROVADO")
+    elif media >= 5.0 and media <= 6.9:
+        print("Situação: RECUPERAÇÃO")
+    else:
+        print("Situação: APROVADO")
+if __name__ == "__main__":
+    calcular_media()
