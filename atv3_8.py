0642c1bab3b0de8a696879fd6855556fbc1d12c9
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-21 17:08:28 -0300

    Update atv3_8.py

--- atv3_8.py
+++ atv3_8.py
@@ -1,3 +1,13 @@
+#Exemplo de while no py
+cont = 0
+while cont <= 50 :
+  print(cont)
+  cont = cont + 5
+ else :
+     print("O valor de cont passou de 50 ", cont)
+//Fim do exemplo
+
+#Exercício38
 cont = 6
 while cont <= 11 :
   print(con)
