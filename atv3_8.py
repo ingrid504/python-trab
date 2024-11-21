b1a578505243f62ad654097723976cb3eb96e6c0
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-21 16:51:16 -0300

    Delete atv3_8.py

--- atv3_8.py
+++
@@ -1,4 +0,0 @@
-# Contagem de 6 a 11
-for i in range(6, 12):  # O range vai de 6 até 11, pois o último valor não é incluído
-    print(i, end=" ")  # O 'end=" "' faz com que os números sejam exibidos na mesma linha
-print("Acabou!")  # Após a contagem, exibe "Acabou!"
