71eca3e82ea2ab6de58f52975b39a8b7955d0cb7
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-22 15:40:25 -0300

    Update atv4_6.py

--- atv4_6.py
+++ atv4_6.py
@@ -1,1 +1,9 @@
+contador = 6
+soma = 0
+
+while contador <= 100:
+    soma += contador  # Corrigido: soma += contador
+    contador += 2  # Corrigido: contador += 2
+
+print("A soma de 6 + 8 + 10 + ... + 100 é:", soma)
 
