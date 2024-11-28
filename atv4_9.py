75b16b58304e79f58e0a5f0d79c525df561adf4e
Ingrid <ingmoroliveira3374@gmail.com>
2024-11-28 14:24:43 -0300

    Update atv4_9.py

--- atv4_9.py
+++ atv4_9.py
@@ -1,1 +1,9 @@
 
+inicio = int(input("Digite o primeiro Valor: "))
+fim = int(input("Digite o último Valor: "))
+incremento = int(input("Digite o incremento: "))
+if incremento == 0:
+    print("O incremento não pode ser zero.")
+else:
+    if incremento > 0 and inicio <= fim:
+        print("Contagem:", end=" ")
