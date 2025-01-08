a7f0818a5f9e9b86dae9731e37ecfd1526bf7dab
Ingrid <ingmoroliveira3374@gmail.com>
2025-01-08 14:35:04 -0300

    Update atv5_5.py

--- atv5_5.py
+++ atv5_5.py
@@ -1,1 +1,8 @@
+# Leitura da temperatura em Celsius
+celsius = float(input("Digite a temperatura em graus Celsius: "))
 
+# Cálculo da temperatura em Fahrenheit
+fahrenheit = celsius * 1.8 + 32
+
+# Exibindo o resultado
+print(f"A temperatura de {celsius}°C é equivalente a {fahrenheit}°F.")
