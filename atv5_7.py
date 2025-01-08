7964d0ef61151318095a3efd0c433e502c631109
Ingrid <ingmoroliveira3374@gmail.com>
2025-01-08 14:39:13 -0300

    Create atv5_7.py

---
+++ atv5_7.py
@@ -0,0 +1,10 @@
+# Leitura de um número
+numero = int(input("Digite um número: "))
+
+# Cálculos do antecessor e do sucessor
+antecessor = numero - 1
+sucessor = numero + 1
+
+# Exibindo os resultados
+print("Antecessor",antecessor)
+print("Sucessor",sucessor)
