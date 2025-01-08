0e84fe4944ec4ccfffe0e87e05146f680a6d6b52
Ingrid <ingmoroliveira3374@gmail.com>
2025-01-08 14:38:14 -0300

    Create atv5_6.py

---
+++ atv5_6.py
@@ -0,0 +1,15 @@
+# Leitura de dois números
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Cálculos das operações
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+# Exibindo os resultados das operações
+print("soma=", soma)
+print("Subtração=", subtracao)
+print("Multiplicação=", multiplicacao)
+print("Divisão=", divisao)
