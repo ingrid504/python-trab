a4f78d1bc3780e7c6af5cec8566170d31622bd8a
Ingrid <ingmoroliveira3374@gmail.com>
2025-01-08 14:39:40 -0300

    Create atv5_8.py

---
+++ atv5_8.py
@@ -0,0 +1,11 @@
+# Leitura de um número
+numero = float(input("Digite um número: "))
+
+# Cálculos do dobro e da metade
+dobro = numero * 2
+metade = numero / 2
+
+# Exibindo os resultados
+print("Numero digitado",num)
+print("Dobro",dobro)
+print("metade",metade)
